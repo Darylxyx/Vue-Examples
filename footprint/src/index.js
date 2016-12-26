d16f66207c04f4764d42a50b395f28f74a9961d1
Darylxyx <921218060@qq.com>
2016-12-26 17:54:55 +0800

    update

--- footprint/src/index.js
+++ footprint/src/index.js
@@ -71,72 +71,11 @@ new Vue({
 				theRequest[paramArr[i].split('=')[0]] = unescape(paramArr[i].split('=')[1]);
 			}
 			return theRequest;
-		},
-		wxShare(data) {
-			// data = JSON.parse(data);
-			wx.config({
-				appId: data.appId,
-				timestamp: data.timestamp,
-				nonceStr: data.nonceStr,
-				signature: data.signature,
-				debug: false,
-				jsApiList: ['onMenuShareTimeline', 'onMenuShareAppMessage', 'onMenuShareQQ']
-			});
-
-			wx.ready(() => {
-				wx.onMenuShareTimeline({ //分享朋友圈
-				    title: 'shareTitle', // 分享标题
-				    link: window.location.href, // 分享链接
-				    imgUrl: 'http://rm-dragon-cdn.kankanapp.com.cn/red-package/act_log', // 分享图标
-				    success: function () { 
-				        // 用户确认分享后执行的回调函数
-				    },
-				    cancel: function () { 
-				        // 用户取消分享后执行的回调函数
-				    }
-				});
-
-				wx.onMenuShareAppMessage({ //分享朋友
-				    title: 'shareTitle', // 分享标题
-				    desc: '分享一个来自探趣的精彩瞬间，点击链接查看更多', // 分享描述
-				    link: window.location.href, // 分享链接
-				    imgUrl: 'http://rm-dragon-cdn.kankanapp.com.cn/red-package/act_log', // 分享图标
-				    type: '', // 分享类型,music、video或link，不填默认为link
-				    dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
-				    success: function () { 
-				        // 用户确认分享后执行的回调函数
-				    },
-				    cancel: function () { 
-				        // 用户取消分享后执行的回调函数
-				    }
-				});
-
-				wx.onMenuShareQQ({
-				    title: 'shareTitle', // 分享标题
-				    desc: '分享一个来自探趣的精彩瞬间，点击链接查看更多', // 分享描述
-				    link: window.location.href, // 分享链接
-				    imgUrl: 'http://rm-dragon-cdn.kankanapp.com.cn/red-package/act_log', // 分享图标http://rm-dragon-cdn.kankanapp.com.cn/red-package/act_log
-				    success: function () { 
-				       // 用户确认分享后执行的回调函数
-				    },
-				    cancel: function () { 
-				       // 用户取消分享后执行的回调函数
-				    }
-				});
-			});
 		}
 	},
 	created() {
 		this.params = this.queryParams() || {};
 		this.params.userId = this.params.userId || '';
 		this.params.locale = this.params.locale || 'zh_CN';
-		// console.log(this.params);
-
-		global.server({
-			url: encodeURIComponent(window.location.href)
-		}, 'https://h5.kankanapp.com.cn/wechatapi/', (res) => {
-			// console.log(JSON.stringify(res));
-			this.wxShare(res);
-		});
 	}
 });

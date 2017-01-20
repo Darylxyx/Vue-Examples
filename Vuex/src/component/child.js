bb95989b5335e6f794f6aa004576326a1ec30ddf
Darylxyx <921218060@qq.com>
2017-01-20 10:28:29 +0800

    update

--- Vuex/src/component/child.js
+++ Vuex/src/component/child.js
@@ -6,12 +6,19 @@ let { mapState } = Vuex;
 console.log(mapState);
 
 const Child = {
-	template: `<h1>Child Component is {{childCount}}</h1>`,
-	computed: {
-		childCount() {
-			return this.$store.state.count;
+	template: `<div>
+				<h1>Child Component is {{count}}</h1>
+				<h1>{{stateA}}</h1>
+				<h1>{{stateAdd}}</h1>
+			  </div>`,
+	// computed: mapState(['count', 'stateA', 'stateB', 'stateC'])
+	computed: mapState({
+		count: state => state.count,
+		stateA: 'stateA',
+		stateAdd(state) {
+			return state.stateB + state.stateC;
 		}
-	}
+	})
 };
 
 export default Child;

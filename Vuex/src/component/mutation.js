7d9dfaddb6b50963d5db25484f93c69cd84a76eb
Darylxyx <921218060@qq.com>
2017-01-25 11:26:43 +0800

    update

--- Vuex/src/component/mutation.js
+++ Vuex/src/component/mutation.js
@@ -1,15 +1,27 @@
 import Vue from 'vue/dist/vue.js';
+let { mapState } = Vuex;
 
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
 	state: {
-		count: 0
+		count: 0,
+		obj: {
+			a: 'a',
+			b: 'b',
+			c: 'c'
+		}
 	},
 	mutations: {
 		addCount(state, data) {
 			state.count += data.num;
 			state.count += data.text;
+		},
+		addObj(state) {
+			// console.log(state);
+			// mutation中对对象添加属性，不可在原对象上添加
+			state.obj = {...state.obj, d: 'd'};
+			console.log(state.obj);
 		}
 	}
 });
@@ -19,15 +31,32 @@ store.commit('addCount', {
 	text: '*'
 });
 
+store.commit('addObj');
+
 // console.log(store.state.count);
 
 const Mutations = {
-	template: `<h1>Mutations count is {{count}}</h1>`,
-	computed: {
-		count() {
-			return store.state.count;
+	template: `<div>
+				<h1>Mutations count is {{count}}</h1>
+				<h1>Mutations obj is {{obj}}</h1>
+			  </div>`,
+	computed: mapState({
+		count: 'count',
+		obj(state) {
+			console.log(state);
+			// 这里的state为根节点state，所以需要state单一
+
+			let str = [];
+
+			for (let o in store.state.obj) {
+				str.push(store.state.obj[o]);
+			}
+
+			str = str.join('，')
+
+			return str;
 		}
-	}
+	})
 };
 
 export default Mutations;

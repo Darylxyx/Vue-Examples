c27ac3ec0c136ddef27098ce27f1745b6b7d89b8
Darylxyx <921218060@qq.com>
2017-02-13 10:37:21 +0800

    update

--- Vuex/src/component/modules.js
+++ Vuex/src/component/modules.js
@@ -4,6 +4,25 @@ const moduleA = {
 	state: {
 		ma1: 'ma1',
 		ma2: 'ma2'
+	},
+	getters: {
+		maGetter(state, getters, rootState) {
+			console.log('moduleA getter', state, getters);
+			console.log('moduleA getter', rootState);
+			return 'A';
+		}
+	},
+	mutations: {
+		maMutation(state) { // 模块内mutation只接收本地state这一个参数
+			console.log('moduleA mutation', state);
+		}
+	},
+	actions: {
+		maAction(context) {
+			console.log('moduleA action', context);
+			context.commit('maMutation');
+			context.commit('mbMutation');
+		}
 	}
 };
 
@@ -11,6 +30,16 @@ const moduleB = {
 	state: {
 		mb1: 'mb1',
 		mb2: 'mb2'
+	},
+	getters: {
+		mbGetter() {
+			return 'B';
+		}
+	},
+	mutations: {
+		mbMutation(state) {
+			console.log('moduleB mutation', state);
+		}
 	}
 };
 
@@ -30,3 +59,6 @@ const store = new Vuex.Store({
 
 console.log('ModuleA store1 : ', store.state.a.ma1);
 
+console.log('ModuleA getters: ', store.getters);
+
+store.dispatch('maAction');

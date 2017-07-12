45590fe10fac067b11278ba56f33d8b6b8717e0b
Darylxyx <921218060@qq.com>
2017-07-12 10:31:24 +0800

    update

--- Component/src/component/Father.vue
+++ Component/src/component/Father.vue
@@ -5,16 +5,23 @@
 			<p>子组件内容</p>
 			<p>子组件更多内容</p>
 		</child>
+		<br/>
+		<h1>layout 组件标题</h1>
+		<layout>
+
+		</layout>
 	</div>
 </template>
 
 <script>
 
 import child from './child.vue';
+import layout from './layOut.vue';
 
 export default {
 	components: {
-		child
+		child,
+		layout
 	}
 }
 
---
+++ Component/src/component/layOut.vue
@@ -0,0 +1,10 @@
+<template>
+	<div class='lay-out'>
+		<header>
+			<slot name='header'></slot>
+		</header>
+		<footer>
+			<slot name='footer'></slot>
+		</footer>
+	</div>
+</template>

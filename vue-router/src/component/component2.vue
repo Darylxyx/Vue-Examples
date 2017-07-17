1ec61609f0abead6c7edd203c99c5d4c77e02396
Darylxyx <921218060@qq.com>
2017-07-17 18:19:33 +0800

    update

---
+++ vue-router/src/component/component2.vue
@@ -0,0 +1,13 @@
+<template>
+	<div class='box-yellow'></div>
+</template>
+
+<style>
+
+.box-yellow {
+	width: 200px;
+	height: 200px;
+	background-color: yellow;
+}
+
+</style>
---
+++ vue-router/src/component/prop.vue
@@ -0,0 +1,15 @@
+<template>
+	<ul>
+		<slot v-for='item in list' :text='item'>
+			<li class='default-list-style'>{{item}}</li>
+		</slot>
+	</ul>
+</template>
+
+<script>
+
+export default {
+	props: ['list']
+}
+
+</script>

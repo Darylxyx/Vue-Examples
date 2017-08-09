779dd5fbf40e5a1964827e306e66ee039ac75f00
Darylxyx <921218060@qq.com>
2017-08-09 16:39:46 +0800

    update

--- MVVM/Observer/Observer.js
+++ MVVM/Observer/Observer.js
@@ -1,18 +1,35 @@
 var saleOffices = {};
 
-saleOffices.clientList = [];
+saleOffices.clientList = {};
 
-saleOffices.listen = function(fn) {
-	this.clientList.push(fn);
+saleOffices.listen = function(key, fn) {
+	if (!this.clientList[key]) {
+		this.clientList[key] = [];
+	}
+	this.clientList[key].push(fn);
 };
 
 saleOffices.trigger = function() {
-	for (var i = 0, max = this.clientList.length; i < max; i ++) {
-		var fn = this.clientList[i];
-		fn.apply(this, arguments);
-	}
+	var key = Array.prototype.shift.call(arguments),
+		arg = arguments,
+		fns = this.clientList[key],
+		_this = this;
+
+	
+
+	fns.forEach(function(fn, index) {
+		fn.apply(_this, arg);
+	});
 };
 
-saleOffices.listen(function(price, suqareMeter) {
-	
+saleOffices.listen('80平', function(price) {
+	console.log(price);
 });
+
+// saleOffices.listen(function(price, suqareMeter) {
+// 	console.log(price);
+// 	console.log(suqareMeter);
+// });
+
+saleOffices.trigger('80平', 88);
+// saleOffices.trigger(200, 110);

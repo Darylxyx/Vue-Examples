a14d8e98d0224d178c234ac67c464c83b746f8a2
Darylxyx <921218060@qq.com>
2017-09-28 16:36:15 +0800

    updte

--- abstract/src/component/Keyboard.vue
+++ abstract/src/component/Keyboard.vue
@@ -1,27 +1,33 @@
 <template>
 	<div class='kb-content'>
 		<div class='kb-left'>
-			<div v-for="(item, index) in keyArr" class='kb-left-item b-r' :class="index < 9 && 'b-b'"></div>
+			<div v-for="(item, index) in keyArr" class='kb-item kb-left-item b-r' :class="index < 9 && 'b-b'" :style="{height: itemH+'px', lineHeight: itemH+'px'}">{{item}}</div>
 		</div>
 		<div class='kb-right'>
-			<div class='kb-right-item k-cancel b-b'></div>
-			<div class='kb-right-item k-submit'></div>
+			<div class='kb-item kb-right-item k-cancel b-b' :style="{height: itemH*2+'px', lineHeight: itemH*2+'px'}"></div>
+			<div class='kb-item kb-right-item k-submit' :style="{height: itemH*2+'px', lineHeight: itemH*2+'px'}">确定</div>
 		</div>
 	</div>
 </template>
-
 <script>
-
 export default {
 	data() {
 		return {
-			keyArr: [1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 0, 'B']
+			winW: window.innerWidth,
+			keyArr: [1, 2, 3, 4, 5, 6, 7, 8, 9, 'X', 0, 'B']
 		};
+	},
+
+	computed: {
+		itemH() {
+			return this.winW / 4 * 0.66;
+		},
+		totalH() {
+			return this.itemH * 4 + 1;
+		}
 	}
 }
-
 </script>
-
 <style>
 .kb-content {
 	width: 100%;
@@ -44,35 +50,45 @@ export default {
 	overflow: hidden;
 }
 
-.kb-left-item {
-	width: 33.33%;
-	padding-bottom: 22.22%;
+.kb-item {
+	font-size: 28px;
 	position: relative;
 	float: left;
-	background-color: #F2F3F4;
 	-moz-box-sizing: border-box;
 	box-sizing: border-box;
+	text-align: center;
+	/*cursor: pointer;*/
+}
+
+.kb-item:active {
+	background-color: #abdef3;
+}
+
+.kb-left-item {
+	width: 33.33%;
+	background-color: #fff;
 }
 
 .kb-right-item {
 	width: 100%;
-	padding-bottom: 133.33%;
-	position: relative;
-	float: left;
-	-moz-box-sizing: border-box;
-	box-sizing: border-box;
 }
 
 .k-cancel {
-	background-color: #F2F3F4;
+	background-color: #fff;
 }
 
 .k-submit {
+	font-size: 18px;
+	color: #fff;
 	background-color: #244E94;
 }
 
+.k-submit:active {
+	background-color: #256eac;
+}
+
 .b-t {
-	border-top: 1px soild #E0E1E2
+	border-top: 1px soild #E0E1E2;
 }
 
 .b-b {
@@ -86,5 +102,4 @@ export default {
 .b-r {
 	border-right: 1px solid #E0E1E2;
 }
-
 </style>
